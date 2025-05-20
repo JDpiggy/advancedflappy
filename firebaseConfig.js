9451e77f96cfcc1ee2c6b742d72b7b9bec06d79a
JDpiggy <jasoncpumpkin247@gmail.com>
2025-05-20 09:33:45 -0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyChKOZoDgFZdxmPCARFnv4mtOQV_QHB2R8",
+  authDomain: "flappybirdlali.firebaseapp.com",
+  projectId: "flappybirdlali",
+  storageBucket: "flappybirdlali.firebasestorage.app",
+  messagingSenderId: "195855441867",
+  appId: "1:195855441867:web:f7dabb47c4b112c638604e",
+  measurementId: "G-1F9T7BFJG6"
+};
