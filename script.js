dd36f6e8208728e75a772863a1d31d959ce389eb
JDpiggy <jasoncpumpkin247@gmail.com>
2025-05-23 11:38:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -181,8 +181,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const MIN_OBSTACLE_SEGMENT_HEIGHT = 40;
     const OBSTACLE_VERTICAL_MOVEMENT_MAX_OFFSET = 60; const OBSTACLE_VERTICAL_SPEED = 0.45;
     const OBSTACLE_TYPES = {
-        beaker:    { img: beakerObstacleImg,    visualWidth: 120, effectiveWidth: 20,  hitboxInsetX: 15, hitboxInsetYGapEdge: 30 },
-        ruler:     { img: rulerObstacleImg,     visualWidth: 60,  effectiveWidth: 15,  hitboxInsetX: 5,  hitboxInsetYGapEdge: 20  },
+        beaker:    { img: beakerObstacleImg,    visualWidth: 120, effectiveWidth: 10,  hitboxInsetX: 15, hitboxInsetYGapEdge: 30 },
+        ruler:     { img: rulerObstacleImg,     visualWidth: 60,  effectiveWidth: 112,  hitboxInsetX: 5,  hitboxInsetYGapEdge: 20  },
         bookstack: { img: bookstackObstacleImg, visualWidth: 150, effectiveWidth: 120, hitboxInsetX: 25, hitboxInsetYGapEdge: 100 }
     };
     const SHIELD_POWERUP_SIZE = 70; const FUEL_POWERUP_SIZE = 100;
